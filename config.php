e755ff98b935cf9c753bb55fefe40d28f73d3c5f
YoilyL <yoilychat@gmail.com>
2017-04-04 21:56:43 +0200

    changed config setup

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+	'token' => 'bot token',
+	'db_host' => 'localhost',
+	'db_username' => 'username',
+	'db_pass' => 'password',
+	'db_name' => 'database',
+	'g_apikey' => 'google api key',
+	
+	];
---
+++ zmanimbot.php
@@ -0,0 +1,3 @@
+<?php
+$config = require('config.php');
+define('config', $config);			//will only work from php 7.0 as config is an array
